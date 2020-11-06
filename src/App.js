b93cd5d44e133df816b3fcf0cb54712b764fa3e5
DavidHall-Code <david_m_hall@outlook.com>
2020-11-06 12:04:11 -0500

    Created profile.js to link to the CharacterCard

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import styled from 'styled-components'
 import './App.css';
-
+import CharacterCard from './components/Character'
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  const [data, setData] = useState([])
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect (() => {
+    axios
+      .get ('https://swapi.dev/api/people/')
+      .then ((res) => {
+        console.log('Here is your requested API data: ', res.data.results)
+        setData (res.data.results)
+      })
+      .catch ((err) => {
+        console.log('An ERROR has populated: ', err)
+      })
+  }, [])
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <CharacterCard data={data} />
     </div>
   );
 }
