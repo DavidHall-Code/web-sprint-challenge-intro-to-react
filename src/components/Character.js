8444009252e322d65ba5e4a0d6c9419af3d3c3e2
DavidHall-Code <david_m_hall@outlook.com>
2020-11-06 12:24:47 -0500

    Setup character.js, console does show that the data is being pulled from the API. going to work on styleing

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,2 +1,17 @@
 import React from 'react'
 import Profile from './Profile'
+
+
+const CharacterCard = (props) => {
+    const {data} = props
+
+return (
+    <div className='character-profile'>
+        {data.map ((element, index) => {
+            return <Profile key={index} name={element.name} birthYear={element.birth_year} />
+        })}
+    </div>
+)
+}
+
+export default CharacterCard
