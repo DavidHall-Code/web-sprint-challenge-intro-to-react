f60e9330c6cb46cf3d694d6aeb1259f3be1945ad
DavidHall-Code <david_m_hall@outlook.com>
2020-11-06 13:09:38 -0500

    Met MVP on project

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-
+import image from '../images/space.jpg'
 
 
 const Profile = (props) => {
@@ -16,26 +16,31 @@ return (
 }
 
 const P1 = styled.h1 `
-color: white;
+color: #d5f6f7;
 text-transform: uppercase;
 text-align: center;
 font-family: 'Comic Sans MS', cursive;
-padding: 0.5rem;
+padding: 1rem;
 letter-spacing = 2px;
 `;
 
-const P2 = styled.h2 `
+const P2 = styled(P1) `
 font-family: 'Courier New', cursive;
 color: #A52A2A;
-font-size: 150%;
+font-size: 2rem;
+text-shadow: 1px 1px white;
 `;
 
 const Plate = styled.div `
+background-image: url(${image});
+background-size: cover;
 border: 1px solid gold;
 width: 90%;
 margin: 20px 50px;
-
-`
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+`;
 
 
 export default Profile
